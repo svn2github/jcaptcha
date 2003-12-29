4519de569dc9f18633174cbb17966fdfb41593b7
magarrigue <magarrigue@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2003-12-29 16:11:51 +0000

    cleanning
    git-svn-id: http://svn.code.sf.net/p/jcaptcha/code/trunk@8 eaf2ec09-4c2e-0410-84d0-fe0bfca4863a

--- core/src/java/com/octo/captcha/engine/AlternateFactoryInitializer.java
+++ core/src/java/com/octo/captcha/engine/AlternateFactoryInitializer.java
@@ -7,10 +7,10 @@ import com.octo.captcha.pix.gimpy.wordtoimages.AbstractTextPaster;
 import com.octo.captcha.pix.gimpy.wordtoimages.AbstractBackgroundGenerator;
 import com.octo.captcha.pix.gimpy.wordtoimages.AbstractFontGenerator;
 import com.octo.captcha.pix.gimpy.wordtoimages.ComposedWordToImage;
+import com.octo.captcha.pix.gimpy.wordtoimages.textpasters.DoubleTextPaster;
 import com.octo.captcha.pix.gimpy.wordtoimages.fontgenerator.TwistedRandomFontGenerator;
 import com.octo.captcha.pix.gimpy.wordtoimages.backgroundgenerators.GradientBackgroundGenerator;
 import com.octo.captcha.pix.gimpy.wordtoimages.backgroundgenerators.FunkyBackgroundGenerator;
-import com.octo.captcha.pix.gimpy.wordtoimages.textpasters.RotatingTextPaster;
 import com.octo.captcha.pix.gimpy.WordGenerator;
 import com.octo.captcha.pix.gimpy.WordToImage;
 import com.octo.captcha.pix.gimpy.GimpyFactory;
@@ -24,7 +24,7 @@ import com.octo.captcha.pix.gimpy.wordgenerators.FileDictionnary;
  */
 public class AlternateFactoryInitializer extends FactoryInitializer {
     public static PixCaptchaFactory getPixCaptchaFactory(){
-                 AbstractTextPaster paster = new RotatingTextPaster(new Integer(8),new Integer(8));
+                 AbstractTextPaster paster = new DoubleTextPaster(new Integer(8),new Integer(8));
             AbstractBackgroundGenerator back = new FunkyBackgroundGenerator(new Integer(50),new Integer(100));
             AbstractFontGenerator font = new TwistedRandomFontGenerator(new Integer(12));
             WordGenerator words = new DictionaryWordGenerator(new FileDictionnary("toddlist"));
