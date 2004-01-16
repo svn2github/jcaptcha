f580e7e309ac3319eafd50d4562c058ea2e0fbe0
mgandin <mgandin@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2004-01-16 14:01:14 +0000

    by mgandin: Adding more test to the jcaptcha core
    git-svn-id: http://svn.code.sf.net/p/jcaptcha/code/trunk@44 eaf2ec09-4c2e-0410-84d0-fe0bfca4863a

---
+++ core/src/test/com/octo/captcha/pix/gimpy/wordtoimages/textpasters/SimpleTextPasterTest.java
@@ -0,0 +1,58 @@
+package com.octo.captcha.pix.gimpy.wordtoimages.textpasters;
+
+import java.awt.image.BufferedImage;
+import java.text.AttributedString;
+
+import com.octo.captcha.CaptchaException;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>Description: </p>
+ * @author <a href="mailto:mga@octo.com">Mathieu Gandin</a>
+ * @version 1.0
+ */
+public class SimpleTextPasterTest extends TestCase {
+
+    private SimpleTextPaster simpleTextPaster;
+    private Integer minAcceptedWordLength = new Integer(10);
+    private Integer maxAcceptedWordLength = new Integer(15);
+
+    /**
+     * Constructor for SimpleTextPasterTest.
+     * @param name
+     */
+    public SimpleTextPasterTest(String name) {
+        super(name);
+    }
+
+    public void setUp() {
+        this.simpleTextPaster = new SimpleTextPaster(
+            this.maxAcceptedWordLength,this.minAcceptedWordLength);
+    }
+
+    public void testPasteText() {
+        BufferedImage testBufferedImage = new BufferedImage(100,50,BufferedImage.TYPE_INT_RGB);
+        AttributedString testAttributedString = new AttributedString("test");
+        BufferedImage test = null;
+        try {
+            test = this.simpleTextPaster.pasteText(testBufferedImage,testAttributedString);
+        } catch (CaptchaException e) {
+            assertNotNull(e);
+        }
+        assertNotNull(test);
+        assertEquals(testBufferedImage.getHeight(),test.getHeight());
+        assertEquals(testBufferedImage.getWidth(),test.getWidth());
+    }
+
+    public void testGetMaxAcceptedWordLenght() {
+        assertEquals(this.maxAcceptedWordLength.intValue(),
+            this.simpleTextPaster.getMaxAcceptedWordLenght());
+    }
+
+    public void testGetMinAcceptedWordLenght() {
+        assertEquals(this.minAcceptedWordLength.intValue(),
+            this.simpleTextPaster.getMinAcceptedWordLenght());
+    }
+
+}
