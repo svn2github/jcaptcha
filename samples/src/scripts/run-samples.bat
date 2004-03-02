8dd55781f6cd20d051a7edc3c3d8d012264547fc
mgandin <mgandin@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2004-03-02 10:15:25 +0000

    by mgandin: modification of script that run samples
    git-svn-id: http://svn.code.sf.net/p/jcaptcha/code/trunk@111 eaf2ec09-4c2e-0410-84d0-fe0bfca4863a

--- samples/src/scripts/run-samples.bat
+++ samples/src/scripts/run-samples.bat
@@ -1,4 +1,5 @@
 set CLASSPATH=%CLASSPATH%;../jcaptcha-core-1.0-beta2-SNAPSHOT/jcaptcha-core-1.0-beta2-SNAPSHOT.jar;
 set CLASSPATH=%CLASSPATH%;jcaptcha-samples-1.0-beta2-SNAPSHOT.jar
 java com.octo.captcha.image.utils.ImageCaptchaToJPEG com.octo.captcha.image.gimpy.BasicGimpyEngine . 10
+java com.octo.captcha.image.utils.SimpleImageCaptchaToJPEG
 pause
--- samples/src/scripts/run-samples.sh
+++ samples/src/scripts/run-samples.sh
@@ -1,3 +1,4 @@
 export CLASSPATH=$CLASSPATH:../jcaptcha-core-1.0-beta2-SNAPSHOT/jcaptcha-core-1.0-beta2-SNAPSHOT.jar
 export CLASSPATH=$CLASSPATH:jcaptcha-samples-1.0-beta2-SNAPSHOT.jar
-java com.octo.captcha.image.utils.ImageCaptchaToJPEG com.octo.captcha.image.gimpy.BasicGimpyEngine . 10
+java com.octo.captcha.image.utils.ImageCaptchaToJPEG com.octo.captcha.image.gimpy.SimpleListImageCaptchaEngine . 10
+java com.octo.captcha.image.utils.SimpleImageCaptchaToJPEG
