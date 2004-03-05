85080a29a2b2c5e21a71db253460e5d9c20e04b1
mgandin <mgandin@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2004-03-05 09:30:23 +0000

    by mgandin: modification to script, in order to prepare the beta2 release
    git-svn-id: http://svn.code.sf.net/p/jcaptcha/code/trunk@113 eaf2ec09-4c2e-0410-84d0-fe0bfca4863a

--- samples/src/scripts/run-samples.bat
+++ samples/src/scripts/run-samples.bat
@@ -1,5 +1,5 @@
-set CLASSPATH=%CLASSPATH%;../jcaptcha-core-1.0-beta2-SNAPSHOT/jcaptcha-core-1.0-beta2-SNAPSHOT.jar;
-set CLASSPATH=%CLASSPATH%;jcaptcha-samples-1.0-beta2-SNAPSHOT.jar
+set CLASSPATH=%CLASSPATH%;../jcaptcha-core-1.0-beta2/jcaptcha-core-1.0-beta2.jar;
+set CLASSPATH=%CLASSPATH%;jcaptcha-samples-1.0-beta2.jar
 java com.octo.captcha.image.utils.ImageCaptchaToJPEG com.octo.captcha.image.gimpy.BasicGimpyEngine . 10
 java com.octo.captcha.image.utils.SimpleImageCaptchaToJPEG
 pause
--- samples/src/scripts/run-samples.sh
+++ samples/src/scripts/run-samples.sh
@@ -1,4 +1,4 @@
-export CLASSPATH=$CLASSPATH:../jcaptcha-core-1.0-beta2-SNAPSHOT/jcaptcha-core-1.0-beta2-SNAPSHOT.jar
-export CLASSPATH=$CLASSPATH:jcaptcha-samples-1.0-beta2-SNAPSHOT.jar
+export CLASSPATH=$CLASSPATH:../jcaptcha-core-1.0-beta2/jcaptcha-core-1.0-beta2.jar
+export CLASSPATH=$CLASSPATH:jcaptcha-samples-1.0-beta2.jar
 java com.octo.captcha.image.utils.ImageCaptchaToJPEG com.octo.captcha.image.gimpy.SimpleListImageCaptchaEngine . 10
 java com.octo.captcha.image.utils.SimpleImageCaptchaToJPEG
