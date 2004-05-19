d994013e02d83ab33e2a274f72e93acc93c19ce2
magarrigue <magarrigue@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2004-05-19 07:02:08 +0000

    Adding tests for toolkit factory
    git-svn-id: http://svn.code.sf.net/p/jcaptcha/code/trunk@145 eaf2ec09-4c2e-0410-84d0-fe0bfca4863a

--- jcaptcha/core/src/test/com/octo/captcha/component/image/utils/ToolkitFactoryTest.java
+++ jcaptcha/core/src/test/com/octo/captcha/component/image/utils/ToolkitFactoryTest.java
@@ -21,8 +21,22 @@ public class ToolkitFactoryTest extends TestCase {
         super(name);
     }
 
-    public void testGetToolkit() {
+    public void testGetaDefaultToolkit() {
         assertTrue(ToolkitFactory.getToolkit() instanceof Toolkit);
     }
 
+    public void testGetCustomToolkit(){
+        System.setProperty(ToolkitFactory.TOOLKIT_IMPL,MockToolkit.class.getName());
+        assertTrue(ToolkitFactory.getToolkit() instanceof MockToolkit);
+        System.setProperty(ToolkitFactory.TOOLKIT_IMPL,"toto");
+        try
+        {
+            ToolkitFactory.getToolkit();
+            fail("should throw an exception");
+        } catch (Exception e)
+        {
+            //assertTrue();
+        }
+
+    }
 }
