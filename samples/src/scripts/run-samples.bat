1c581bd9a69ccfa47c5276ea50b8d0b5f53a39bc
magarrigue <magarrigue@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2005-04-19 21:56:39 +0000

    Cleanning
    git-svn-id: http://svn.code.sf.net/p/jcaptcha/code/trunk@228 eaf2ec09-4c2e-0410-84d0-fe0bfca4863a

--- samples/src/scripts/run-samples.bat
+++
@@ -1,5 +0,0 @@
-set CLASSPATH=%CLASSPATH%;../jcaptcha-core-1.0-beta2/jcaptcha-core-1.0-beta2.jar;
-set CLASSPATH=%CLASSPATH%;jcaptcha-samples-1.0-beta2.jar
-java com.octo.captcha.image.utils.ImageCaptchaToJPEG com.octo.captcha.image.gimpy.BasicGimpyEngine . 10
-java com.octo.captcha.image.utils.SimpleImageCaptchaToJPEG
-pause
