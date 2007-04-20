d9e75ee8be1d45fbc0e7f53b975d664fa918e8be
antoineveret <antoineveret@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2007-04-20 16:32:04 +0000

    Integraton test migration
    git-svn-id: http://svn.code.sf.net/p/jcaptcha/code/trunk@394 eaf2ec09-4c2e-0410-84d0-fe0bfca4863a

---
+++ testing/integration-test/src/test/java/com/octo/captcha/engine/bufferedengine/buffer/MemoryCaptchaBufferTest.java
@@ -0,0 +1,24 @@
+/*
+ * JCaptcha, the open source java framework for captcha definition and integration
+ * Copyright (c)  2007 jcaptcha.net. All Rights Reserved.
+ * See the LICENSE.txt file distributed with this package.
+ */
+
+/*
+ * jcaptcha, the open source java framework for captcha definition and integration
+ * copyright (c)  2007 jcaptcha.net. All Rights Reserved.
+ * See the LICENSE.txt file distributed with this package.
+ */
+package com.octo.captcha.engine.bufferedengine.buffer;
+
+
+public class MemoryCaptchaBufferTest extends CaptchaBufferTestAbstract {
+
+    /**
+     * @see com.octo.captcha.engine.bufferedengine.buffer.CaptchaBufferTestAbstract#getBuffer()
+     */
+    public CaptchaBuffer getBuffer() {
+        return new MemoryCaptchaBuffer();
+    }
+
+}
